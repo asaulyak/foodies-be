6de3d58fdf9db2ffba61c4b4b5afcd65319790c9
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2024-09-03 21:42:42 +0300

    Feat/helth check (#31)
    * added health endpoints
    
    * added ssl db support
    
    ---------
    
    Co-authored-by: Eugene Asaulyak <eugene.asaulyak@gmail.com>

--- src/common/data/sequelize.js
+++ src/common/data/sequelize.js
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize';
 import { ENV_CONFIG } from '../config/index.js';
 
-const { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME, DB_PORT } = ENV_CONFIG;
+const { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME, DB_PORT, NODE_ENV } = ENV_CONFIG;
 
 export const sequelize = new Sequelize({
   dialect: 'postgres',
   dialectOptions: {
-    useNativeUUID: true
+    useNativeUUID: true,
+    ssl: NODE_ENV === 'prod'
   },
   host: DB_HOST,
   username: DB_USER,

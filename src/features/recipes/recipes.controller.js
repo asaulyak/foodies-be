5b2040a36f41f803322271ebaab3c999bcca2f45
codajoker <84983434+codajoker@users.noreply.github.com>
2024-09-04 12:24:04 +0200

    Add controller
    Add controller for create receipt

--- src/features/recipes/recipes.controller.js
+++ src/features/recipes/recipes.controller.js
@@ -20,3 +20,20 @@ export const getById = async (req, res, next) => {
     next(e);
   }
 };
+
+export const createRecipes = async (req, res, next) => {
+  const { name, description, image } = req.body;
+  if (!name || !description || !image) {
+    return next(HttpError(404));
+  }
+  try {
+    const newRecipe = await createRecipes(name, description, image);
+
+    if (!newRecipe) {
+      return next(HttpError(500));
+    }
+    res.status(201).json(newRecipe);
+  } catch (e) {
+    next(e);
+  }
+};

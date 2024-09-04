f2899345ff211def63db7ab31a31e51acb2e7edb
codajoker <84983434+codajoker@users.noreply.github.com>
2024-09-04 12:23:16 +0200

    Add Router
    add Router for create receipt

--- src/features/recipes/recipes.routes.js
+++ src/features/recipes/recipes.routes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { getById } from './recipes.controller.js';
+import { authMiddleware } from '../../common/middleware/auth.middleware.js';
+import { createRecipes } from './recipes.controller.js';
 
 export const recipesRouter = express.Router();
 
 recipesRouter.get('/:id', getById);
+recipesRouter.post('/', authMiddleware, createRecipes);

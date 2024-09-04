a2624e09862fc3899f452bd2b2cddef08da9db71
Andriy Babiy <32708111+AndriyBabiy@users.noreply.github.com>
2024-09-04 16:29:11 +0100

    Added route and contoller for users/current (#38)
    * Added route and contoller for users/current
    
    * removed duplicate route
    
    ---------
    
    Co-authored-by: Eugene Asaulyak <jenya.asaulyak@gmail.com>

--- src/features/users/users.routes.js
+++ src/features/users/users.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { userRegisterSchema } from './schemas/user-register.schema.js';
-import { loginUser, me, registerUser } from './users.controller.js';
+import { getCurrent, loginUser, registerUser } from './users.controller.js';
 import { validateBodyMiddleware } from '../../common/middleware/validate-body.middleware.js';
 import { userLoginSchema } from './schemas/user-login.schema.js';
 import { authMiddleware } from '../../common/middleware/auth.middleware.js';
@@ -9,4 +9,4 @@ export const userRouter = express.Router();
 
 userRouter.post('/signup', validateBodyMiddleware(userRegisterSchema), registerUser);
 userRouter.post('/signin', validateBodyMiddleware(userLoginSchema), loginUser);
-userRouter.get('/me', authMiddleware, me);
+userRouter.get('/current', authMiddleware, getCurrent);

12299f446a3217413b1bd57cfdd865d523d95b67
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-09-05 04:41:25 +0300

    add pagination

--- src/features/categories/categories.service.js
+++ src/features/categories/categories.service.js
@@ -1,5 +1,13 @@
 import { Categories } from '../../common/data/entities/category/categories.entity.js';
 
-export const listCategories = () => {
-  return Categories.findAll();
+export const listCategories = (pagination = {}) => {
+  const { page = 1, limit = 11 } = pagination;
+
+  const normalizedLimit = Number(limit);
+  const offset = (Number(page) - 1) * normalizedLimit;
+  console.log(`Page: ${page}, Limit: ${limit}`);
+  return Categories.findAll({
+    offset,
+    limit: normalizedLimit
+  });
 };

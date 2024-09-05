defd3e6a8fb36c8d1d6f96b02fa2b4261f585ffa
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-09-05 08:18:01 +0300

    added pagination middleware

--- src/features/categories/categories.controller.js
+++ src/features/categories/categories.controller.js
@@ -1,9 +1,10 @@
 import { listCategories } from './categories.service.js';
+import { HttpError } from '../../common/errors/http-error.js';
 
-export const getCategories = async (req, res) => {
+export const getCategories = async (req, res, next) => {
   try {
-    const { page, limit } = req.query;
-    const categories = await listCategories({ page, limit });
+    const { limit, offset } = req.pagination;
+    const categories = await listCategories({ offset, limit });
     res.json(categories);
   } catch (error) {
     next(HttpError(500));

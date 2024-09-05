d254fdbfa878e336ed64fc023c02a1d773aec002
codajoker <84983434+codajoker@users.noreply.github.com>
2024-09-05 21:42:21 +0200

    add swager

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ import { recipesRouter } from './features/recipes/recipes.routes.js';
 import { categoriesRouter } from './features/categories/categories.routes.js';
 import { areasRouter } from './features/areas/areas.routes.js';
 import { ingredientsRouter } from './features/ingredients/ingredients.routes.js';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerOptions from './swagger.json' assert { type: 'json' };
 configEnvVars();
 
 const app = express();
@@ -26,6 +27,7 @@ app.use('/api/categories', categoriesRouter);
 app.use('/api/areas', areasRouter);
 app.use('/api/ingredients', ingredientsRouter);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 app.use((_, res, __) => {
   res.status(404).json({
     status: 'error',

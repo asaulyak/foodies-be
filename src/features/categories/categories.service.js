e659beab039e2de7cae7d532c6d3a6485c990a84
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2024-09-05 08:18:49 +0300

    added pagination middleware (#47)
    Co-authored-by: Eugene Asaulyak <eugene.asaulyak@gmail.com>

--- src/features/categories/categories.service.js
+++ src/features/categories/categories.service.js
@@ -1,12 +1,10 @@
 import { Categories } from '../../common/data/entities/category/categories.entity.js';
 
 export const listCategories = (pagination = {}) => {
-  const { page = 1, limit = 11 } = pagination;
+  const { offset, limit } = pagination;
 
-  const normalizedLimit = Number(limit);
-  const offset = (Number(page) - 1) * normalizedLimit;
   return Categories.findAll({
     offset,
-    limit: normalizedLimit
+    limit
   });
 };

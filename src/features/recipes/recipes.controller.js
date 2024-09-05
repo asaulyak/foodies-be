a9920cb83363abd0875954ac05ad6e8df1c1c371
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2024-09-05 20:47:13 +0300

    added recipes search (#56)
    * added recipes search
    
    * merged with master
    
    ---------
    
    Co-authored-by: Eugene Asaulyak <eugene.asaulyak@gmail.com>

--- src/features/recipes/recipes.controller.js
+++ src/features/recipes/recipes.controller.js
@@ -1,7 +1,16 @@
-import { createRecipes, getPopularRecipes, getRecipeById, removeRecipe } from './recipes.service.js';
+import {
+  createRecipes,
+  getRecipeById,
+  getPopularRecipes,
+  getRecipesByFilter,
+  removeRecipe
+} from './recipes.service.js';
 import { HttpError } from '../../common/errors/http-error.js';
+<<<<<<< HEAD
 import { fn } from 'sequelize';
 import { controllerWrapper } from '../../common/decorators/controller-wrapper.js';
+=======
+>>>>>>> d7dcd95 (added recipes search (#56))
 
 export const getById = controllerWrapper(async (req, res) => {
   const { id } = req.params;
@@ -29,7 +38,42 @@ export const createRecipe = controllerWrapper(async (req, res) => {
   res.status(201).json(newRecipe);
 });
 
+<<<<<<< HEAD
 export const getPopular = controllerWrapper(async (req, res) => {
   const popularRecipes = await getPopularRecipes();
   return res.json(popularRecipes);
 });
+=======
+export const searchRecipes = async (req, res) => {
+  const { categoryId, areaId, ingredientIds } = req.query;
+  const { limit, offset } = req.pagination;
+
+  const recipes = await getRecipesByFilter({ categoryId, areaId, ingredientIds, limit, offset });
+
+  res.json(recipes);
+};
+
+export const getPopular = async (req, res, next) => {
+  try {
+    const popularRecipes = await getPopularRecipes();
+    return res.json(popularRecipes);
+  } catch (e) {
+    next(e);
+  }
+};
+export const deleteRecipe = async (req, res, next) => {
+  const { id } = req.params;
+  if (!id) {
+    return next(HttpError(404));
+  }
+  try {
+    const result = await removeRecipe(id);
+    if (!result) {
+      return next(HttpError(404));
+    }
+    res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+};
+>>>>>>> d7dcd95 (added recipes search (#56))

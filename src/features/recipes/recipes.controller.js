0f61a2f8c0b7e93b7626853f193a343d967e39fc
Andriy Babiy <andriybabiy97@gmail.com>
2024-09-05 19:24:21 +0100

    rebase issue

--- src/features/recipes/recipes.controller.js
+++ src/features/recipes/recipes.controller.js
@@ -6,11 +6,8 @@ import {
   removeRecipe
 } from './recipes.service.js';
 import { HttpError } from '../../common/errors/http-error.js';
-<<<<<<< HEAD
 import { fn } from 'sequelize';
 import { controllerWrapper } from '../../common/decorators/controller-wrapper.js';
-=======
->>>>>>> d7dcd95 (added recipes search (#56))
 
 export const getById = controllerWrapper(async (req, res) => {
   const { id } = req.params;
@@ -38,29 +35,20 @@ export const createRecipe = controllerWrapper(async (req, res) => {
   res.status(201).json(newRecipe);
 });
 
-<<<<<<< HEAD
 export const getPopular = controllerWrapper(async (req, res) => {
   const popularRecipes = await getPopularRecipes();
   return res.json(popularRecipes);
 });
-=======
-export const searchRecipes = async (req, res) => {
+
+export const searchRecipes = controllerWrapper(async (req, res) => {
   const { categoryId, areaId, ingredientIds } = req.query;
   const { limit, offset } = req.pagination;
 
   const recipes = await getRecipesByFilter({ categoryId, areaId, ingredientIds, limit, offset });
 
   res.json(recipes);
-};
+});
 
-export const getPopular = async (req, res, next) => {
-  try {
-    const popularRecipes = await getPopularRecipes();
-    return res.json(popularRecipes);
-  } catch (e) {
-    next(e);
-  }
-};
 export const deleteRecipe = async (req, res, next) => {
   const { id } = req.params;
   if (!id) {
@@ -76,4 +64,3 @@ export const deleteRecipe = async (req, res, next) => {
     next(e);
   }
 };
->>>>>>> d7dcd95 (added recipes search (#56))

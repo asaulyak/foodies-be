ebaf2bb209228c28007feb579659331ebc682f46
yarqui <y.pelykh@gmail.com>
2024-09-06 00:14:41 -0400

    feat: add users/avatar router

--- src/features/users/users.routes.js
+++ src/features/users/users.routes.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import { userRegisterSchema } from './schemas/user-register.schema.js';
-import { getCurrent, loginUser, registerUser, getFollowers, getFollowing, getRecipes } from './users.controller.js';
+import {
+  getCurrent,
+  loginUser,
+  registerUser,
+  getFollowers,
+  getFollowing,
+  getRecipes,
+  updateAvatar
+} from './users.controller.js';
 import { validateBodyMiddleware } from '../../common/middleware/validate-body.middleware.js';
 import { userLoginSchema } from './schemas/user-login.schema.js';
 import { authMiddleware } from '../../common/middleware/auth.middleware.js';
 import { paginationMiddleware } from '../../common/middleware/pagination.middleware.js';
+import upload from '../../common/middleware/upload.middleware.js';
+
+const uploadAvatarMiddleware = upload.single('avatar');
 
 export const userRouter = express.Router();
 
@@ -14,3 +25,4 @@ userRouter.get('/current', authMiddleware, getCurrent);
 userRouter.get('/followers', authMiddleware, paginationMiddleware, getFollowers);
 userRouter.get('/following', authMiddleware, paginationMiddleware, getFollowing);
 userRouter.get('/recipes', authMiddleware, paginationMiddleware, getRecipes);
+userRouter.patch('/avatar', authMiddleware, uploadAvatarMiddleware, updateAvatar);

faea254c05db774168cbee48f5f7da96ca795d04
Kostiantyn Bohdan <136738569+KostBoh@users.noreply.github.com>
2024-09-06 07:56:16 +0300

    add signout feature (#57)

--- src/features/users/users.routes.js
+++ src/features/users/users.routes.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import { userRegisterSchema } from './schemas/user-register.schema.js';
-import { getCurrent, loginUser, registerUser, getFollowers, getFollowing, getRecipes } from './users.controller.js';
+import {
+  getCurrent,
+  loginUser,
+  registerUser,
+  getFollowers,
+  getFollowing,
+  signoutUser,
+  getRecipes
+} from './users.controller.js';
+
 import { validateBodyMiddleware } from '../../common/middleware/validate-body.middleware.js';
 import { userLoginSchema } from './schemas/user-login.schema.js';
 import { authMiddleware } from '../../common/middleware/auth.middleware.js';
@@ -14,3 +23,4 @@ userRouter.get('/current', authMiddleware, getCurrent);
 userRouter.get('/followers', authMiddleware, paginationMiddleware, getFollowers);
 userRouter.get('/following', authMiddleware, paginationMiddleware, getFollowing);
 userRouter.get('/recipes', authMiddleware, paginationMiddleware, getRecipes);
+userRouter.post('/signout', authMiddleware, signoutUser);

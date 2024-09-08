f48d8bf02358fa56024827f64b4030e28a3fd4d2
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-09-08 09:48:00 +0300

    merged with master

--- config/config.js
+++ config/config.js
@@ -12,5 +12,19 @@ export default {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10),
     dialect: 'postgres'
+  },
+  prod: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    }
   }
 };

5a30c8d9a4766a9d96559d17eea0c900448bfa90
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-09-10 10:52:35 +0300

    removed pagination on categories endpoint

--- src/features/categories/categories.routes.js
+++ src/features/categories/categories.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { getCategories } from './categories.controller.js';
-import { paginationMiddleware } from '../../common/middleware/pagination.middleware.js';
 
 export const categoriesRouter = express.Router();
 
-categoriesRouter.get('/', paginationMiddleware, getCategories);
+categoriesRouter.get('/', getCategories);
--- src/features/categories/categories.service.js
+++ src/features/categories/categories.service.js
@@ -1,12 +1,5 @@
 import { Categories } from '../../common/data/entities/category/categories.entity.js';
-import { paginationWrapper } from '../../common/data/pagination.wrapper.js';
 
-export const listCategories = (pagination = {}) => {
-  const { offset, limit, page } = pagination;
-
-  return paginationWrapper(() => Categories, {
-    offset,
-    limit,
-    page
-  });
+export const listCategories = () => {
+  return Categories.findAll();
 };

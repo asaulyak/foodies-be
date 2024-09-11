a747193049a94adf3924863a12578ecfe77a1521
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2024-09-11 19:08:58 +0300

    added thumb upload endpoint (#75)
    Co-authored-by: Eugene Asaulyak <eugene.asaulyak@gmail.com>

--- src/common/helpers/cloudinary.js
+++ src/common/helpers/cloudinary.js
@@ -24,6 +24,19 @@ export const uploadAvatar = async imagePath => {
   }
 };
 
+export const uploadRecipeThumb = async imagePath => {
+  const options = {
+    folder: 'recipe_thumbnails',
+    transformation: [{ height: 400, width: 550 }]
+  };
+
+  try {
+    return await cloudinary.uploader.upload(imagePath, options);
+  } catch ({ status, message }) {
+    throw HttpError(status, message);
+  }
+};
+
 export const deleteAvatar = async avatarUrl => {
   try {
     const avatarPublicId = extractFilename(avatarUrl);
--- src/features/recipes/recipes.routes.js
+++ src/features/recipes/recipes.routes.js
@@ -6,19 +6,25 @@ import {
   getPopular,
   removeFromFavorites,
   createRecipe,
-  addToFavorites
+  addToFavorites,
+  uploadThumb
 } from './recipes.controller.js';
 import { authMiddleware } from '../../common/middleware/auth.middleware.js';
-import { recipesCreateSchema } from './schemas/recipes-create.schemas.js';
+import { recipesCreateSchema } from './schemas/recipes-create.schema.js';
 import { validateBodyMiddleware } from '../../common/middleware/validate-body.middleware.js';
 import { validateQueryStringMiddleware } from '../../common/middleware/validate-query-string.middleware.js';
-import { recipesSearchSchema } from './schemas/recipes-search.schemas.js';
+import { recipesSearchSchema } from './schemas/recipes-search.schema.js';
 import { paginationMiddleware } from '../../common/middleware/pagination.middleware.js';
+import { upload } from '../../common/middleware/upload.middleware.js';
 
 export const recipesRouter = express.Router();
 
+const uploadThumbMiddleware = upload.single('thumb');
+
 recipesRouter.get('/popular', getPopular);
 recipesRouter.get('/search', validateQueryStringMiddleware(recipesSearchSchema), paginationMiddleware, searchRecipes);
+recipesRouter.post('/thumb', authMiddleware, uploadThumbMiddleware, uploadThumb);
+
 recipesRouter.get('/:id', getById);
 recipesRouter.post('/', authMiddleware, validateBodyMiddleware(recipesCreateSchema), createRecipe);
 

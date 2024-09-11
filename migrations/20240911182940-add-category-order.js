1c4d2e85a75907b1e5cdf4eeb56954c63e10c27a
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-09-12 00:05:43 +0300

    added category order

--- migrations/20240911182940-add-category-order.js
+++ migrations/20240911182940-add-category-order.js
@@ -1,8 +1,90 @@
-'use strict';
+const { Sequelize } = require('sequelize');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {},
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('categories', 'order', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    });
 
-  async down(queryInterface, Sequelize) {}
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 0
+      WHERE name = 'Beef';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 1
+      WHERE name = 'Breakfast';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 2
+      WHERE name = 'Dessert';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 3
+      WHERE name = 'Lamb';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 4
+      WHERE name = 'Goat';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 5
+      WHERE name = 'Miscellaneous';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 6
+      WHERE name = 'Pasta';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 7
+      WHERE name = 'Pork';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 8
+      WHERE name = 'Seafood';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 9
+      WHERE name = 'Side';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 10
+      WHERE name = 'Starter';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 11
+      WHERE name = 'Vegetarian';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 12
+      WHERE name = 'Vegan';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 13
+      WHERE name = 'Chicken';
+    `);
+
+    await queryInterface.sequelize.query(`
+      UPDATE categories SET "order" = 14
+      WHERE name = 'Soup';
+    `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('categories', 'order');
+  }
 };

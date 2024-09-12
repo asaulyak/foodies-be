cda4e4ed5a7698da0d67b517a03e21252a0b7b0d
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-09-12 22:05:27 +0300

    added user to review endpoint

--- src/features/reviews/review.service.js
+++ src/features/reviews/review.service.js
@@ -1,5 +1,13 @@
 import { Reviews } from '../../common/data/entities/reviews/reviews.entity.js';
+import { Users } from '../../common/data/entities/users/users.entity.js';
 
 export const listReviews = () => {
-  return Reviews.findAll();
+  return Reviews.findAll({
+    include: [
+      {
+        model: Users,
+        attributes: ['id', 'name']
+      }
+    ]
+  });
 };

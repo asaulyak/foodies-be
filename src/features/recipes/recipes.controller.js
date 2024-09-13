b8e1ef3f8cb509e80eae9dc09677486df513bc2e
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2024-09-13 22:00:17 +0300

    Fix/errors (#83)
    * show 400 errors on prod
    
    * fixed removing from favs
    
    * return id for user info
    
    * send 204 for recipe delete
    
    ---------
    
    Co-authored-by: Eugene Asaulyak <eugene.asaulyak@gmail.com>

--- src/features/recipes/recipes.controller.js
+++ src/features/recipes/recipes.controller.js
@@ -76,6 +76,8 @@ export const deleteRecipe = controllerWrapper(async (req, res, next) => {
   if (!result) {
     return next(HttpError(404));
   }
+
+  res.sendStatus(204);
 });
 
 export const addToFavorites = async (req, res, next) => {
